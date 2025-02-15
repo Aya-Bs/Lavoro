c3d528b1d45cdcf8c2680c462f69a25f6f77502e
Aya Boukhris <eyaboukh@gmail.com>
2025-02-15 14:53:12 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const db = require('./src/config/dbConnection.json');
+const mongo = require('mongoose');
+const http = require('http');
+const server = http.createServer(app,console.log('Server is running on port 3001'));
+mongo.connect(db.url).then(()=> {
+  console.log('Connected to the database');
+}
+).catch((err) => {
+  console.log('Cannot connect to the database', err);
+})
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'twig');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+server.listen(3001);
+
+module.exports = app;
