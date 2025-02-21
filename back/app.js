b6ba36c4fe87ce1fc7e248709b00d813a2b5360c
nousseibakaabi <nousseiba.kaabi@esprit.tn>
2025-02-21 18:25:45 +0100

    Front + back

---
+++ back/app.js
@@ -0,0 +1,88 @@
+require('dotenv').config();
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongo = require('mongoose');
+const http = require('http');
+const bodyparser = require('body-parser');
+const db = require('./config/dbConnection.json');
+const session = require('express-session');
+const transporter = require('./utils/emailConfig'); // Import the transporter from middleware
+const MongoStore = require('connect-mongo');
+const cors = require('cors');
+const hello = require('./models/hello');
+
+// Connect to MongoDB
+mongo
+  .connect(db.url)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const usersRouter = require('./routes/users');
+
+const app = express();
+app.use(cors({
+  origin: 'http://localhost:5173', // Frontend URL
+  credentials: true, // Allow cookies to be sent/received
+}));
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'twig');
+
+// Middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Session configuration
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // Use a secure secret key
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: db.url, // MongoDB connection URL
+      ttl: 24 * 60 * 60, // Session TTL (1 day)
+    }),
+    cookie: {
+      secure: false, // Set to true if using HTTPS
+      httpOnly: true, // Prevent client-side JavaScript from accessing the cookie
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+      sameSite: 'lax', // Prevent CSRF attacks
+    },
+  })
+);
+
+// Routes
+app.use('/users', usersRouter);
+
+// Error handling
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Create server
+const server = http.createServer(app);
+
+// Start server
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
